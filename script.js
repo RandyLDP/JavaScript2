5eebd8c11d0baefcfe931e886e1e4013d13620f2
RandyLDP <80972485+RandyLDP@users.noreply.github.com>
2021-05-11 22:19:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('Hallo Winc Academy studenten')
+// Dit is een grote som
+/*
+function multiply (number, number1){
+    return number * number1
+}
+console.log(multiply(1230941, 1823794  ));
+
+function division (number, number1){
+    return number * number1
+}
+console.log(division(637263, 54));
+*/
